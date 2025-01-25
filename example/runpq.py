d8fae3d21743fc121f7c837d252fef12addd9268
w453908766 <w453908766@126.com>
2025-01-25 18:17:49 +0800

    add ignore_dirs

--- example/runpq.py
+++ example/runpq.py
@@ -6,4 +6,4 @@ from watchrun import watch_dir_cmds
 if __name__ == "__main__":
   argv = ["./ppp.py", "./qqq.py"]
   cmds = [f"{sys.executable} {arg}" for arg in argv]
-  watch_dir_cmds("./dir", 1, cmds)
+  watch_dir_cmds("./dir", 1, cmds, ["./dir/ddd"])
--- watch.py
+++ watch.py
@@ -1,22 +1,44 @@
 #!/usr/bin/python3
 
+from os import path
 from watchdog.events import FileSystemEvent, FileSystemEventHandler
 from watchdog.observers import Observer
 from .debounce import DebounceThread
 from .process_line import ProcessLine
 
+def inDirs(dirs, dir):
+  d0 = path.relpath(dir)
+
+  for d in dirs:
+    if d == path.commonpath([d, d0]):
+      return True
+  return False
+
 class MyEventHandler(FileSystemEventHandler):
-  def __init__(self, delay, terminate_callback, start_callback):
+  def __init__(self, delay, terminate_callback, start_callback, ignore_dirs=[]):
     self.debounce = DebounceThread(delay)
     self.terminate_callback = terminate_callback
     self.start_callback = start_callback
+    self.ignore_dirs = [path.relpath(dir) for dir in ignore_dirs]
 
-  def on_any_event(self, event: FileSystemEvent) -> None:
+  def on_created(self, event: FileSystemEvent) -> None:
+    if inDirs(self.ignore_dirs, event.src_path): return
+
     self.terminate_callback()
     self.debounce.dispose(self.start_callback)
 
-def watch_dir(path, delay, terminate_callback, start_callback):
-  event_handler = MyEventHandler(delay, terminate_callback, start_callback)
+  def on_deleted(self, event: FileSystemEvent) -> None:
+    self.on_created(event)
+
+  def on_modified(self, event: FileSystemEvent) -> None:
+    self.on_created(event)
+
+  def on_moved(self, event: FileSystemEvent) -> None:
+    self.on_created(event)
+
+
+def watch_dir(path, delay, terminate_callback, start_callback, ignore_dirs=[]):
+  event_handler = MyEventHandler(delay, terminate_callback, start_callback, ignore_dirs)
   observer = Observer()
   observer.schedule(event_handler, path, recursive=True)
   observer.start()
@@ -25,8 +47,8 @@ def watch_dir(path, delay, terminate_callback, start_callback):
   except KeyboardInterrupt:
     observer.stop()
 
-def watch_dir_cmds(path, delay, cmds):
+def watch_dir_cmds(path, delay, cmds, ignore_dirs=[]):
   process_line = ProcessLine()
   terminate_callback = lambda: process_line.terminate_process()
   start_callback = lambda: process_line.run_process(cmds)
-  watch_dir(path, delay, terminate_callback, start_callback)
+  watch_dir(path, delay, terminate_callback, start_callback, ignore_dirs)
